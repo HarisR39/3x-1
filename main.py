363d3596ddb723b898ab8d1456a53011e1fd6b0c
HarisR39 <fireplatypus375@gmail.com>
2022-04-04 10:45:04 -0400

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+x = 1
+while x == 1:
+    x = int(input("enter a number: "))
+    while x != 1.0:
+        if x % 2 == 0:
+            x = x/2
+        else:
+            x = x*3+1
+        print(x)
+
