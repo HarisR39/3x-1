03cd955c96ee664b7d3d94cb02893477697c1021
HarisR39 <fireplatypus375@gmail.com>
2022-04-04 13:17:40 -0400

    commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-list = list()
+import matplotlib.pyplot as plt
+list = []
+list1 = []
+y = 0
 x = 1
 while x == 1:
     x = int(input("enter a number: "))
@@ -8,5 +11,10 @@ while x == 1:
         else:
             x = x*3+1
         print(x)
+        y = y + 1
+        list1.append(y)
         list.append(x)
+    plt.plot(list1, list)
+    plt.show()
+
 
