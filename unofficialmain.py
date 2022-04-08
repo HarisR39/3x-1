73b0a93b1d484db9c5c84dca6c8038818db44148
HarisR39 <fireplatypus375@gmail.com>
2022-04-08 10:44:41 -0400

    commit

--- unofficialmain.py
+++ unofficialmain.py
@@ -1,16 +1,18 @@
-import time
-
 b = []
-y = 1
-x = True
-for g in range(50):
-    y= y +1 
-    for z in range(1, y):
-        if y % z != 0:
-            break
-        else:
-            b.append(y)
+x = int(input("enter a number: "))
+for z in range(2, x):
+    for v in range(2, z):
+        if z % v == 0:
+            break 
+        b.append(z)
+for g in range(1, len(b)):
+    for h in range(g, len(b)):
+        if b[g] == b[h]:
+            del b[h]
 print(b)
+    
+
+    
 
 
     
